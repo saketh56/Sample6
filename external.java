ff04f04af7661c9d45a8691eceade090e9259d5f
saketh56 <22h51a0577@cmrcet.ac.in>
2024-12-09 11:56:50 +0530

    Add files via upload

---
+++ external.java
@@ -0,0 +1,20 @@
+public class Prime
+{
+    public static void main(String[] args)
+    {
+        for(int i=2;i<=100;i++)
+        {
+            BooleanisPrime=true;
+            for(int j-2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    System.out.println("Prime");
+                }
+                BooleanisPrime=false;
+                System.out.println("Not prime");
+            }
+        }
+        System.out.println(i);
+    }
+}
